0ed27bbe0194595497e357758c538b27cc314118
Tareq <root@Tareqs-MacBook-Pro.local>
2023-11-02 14:40:01 +0200

    adjust args

--- react-studio-js.d.ts
+++ react-studio-js.d.ts
@@ -1,46 +1,69 @@
 declare module 'react-studio-js'{
 
-    type TArgs =  {
-        ac: any,
-        container: Node,
-        state: string,
-        mono: boolean,
-        samplesPerPixel: number | 500,
-        waveHeight: number | 100,
-        isAutomaticScroll: boolean,
-        timescale: boolean,
-        barGap: number | 1,
-        colors: {
-          waveOutlineColor: string | '#222B36',
-          timeColor: string | 'grey',
-          fadeColor: string | 'black',
-        },
-        controls: null | {
-          show: boolean,
-          width: number | 175,
-          widgets: {
-            collapse: boolean | false,
-          },
-        },
-        annotationList: null | {
-          annotations: [],
-          controls: any,
-          editable: boolean,
-          isContinuousPlay: boolean,
-          linkEndpoints: boolean,
-        },
-    
-        zoomLevels: number[] | [500, 1000, 2000],
-        seekStyle: string | 'fill',
-      }
-    
-    
-    
-    
-    
-    
-        function ReactStudioJS(arg1:TArgs,ee:any):any{}
-    
-    
-        export = ReactStudioJS
-    }
+  type TAnnotation = {
+    id: string;
+    start: number;
+    end: number;
+    lines: string[];
+    lang: string;
+  };
+
+
+type Controls =  {
+  class:string,
+  title:string,
+  action:(annotaion:TAnnotation, i:number,annotations:TAnnotation[], opts:{
+    linkEndpoints?:true | false
+  })=> void
+}[]
+
+
+type TArgs =  {
+    ac: AudioContext ,
+    container: Node,
+    state: 'cursor' | 'fadein' | 'fadeout' | 'select' | 'shift',
+    mono: true | false,
+    samplesPerPixel: number | 500,
+    waveHeight: number | 100,
+    fadeType?: "logarithmic" | 'linear' | 'sCurve' | 'exponential', 
+    isAutomaticScroll: true | false,
+    timescale: true | false,
+    barGap: number | 1,
+    colors: {
+      waveOutlineColor: string | '#222B36',
+      timeColor: string | 'grey',
+      fadeColor: string | 'black',
+    },
+    controls:  {
+      show: true | false,
+      width: number | 175,
+      widgets: {
+        collapse: false,
+        muteOrSolo: true | false,
+        volume: true | false,
+        stereoPan: true | false,
+        remove: true | false,
+      },
+    },
+    annotationList: null | {
+      annotations: [],
+      controls: Controls,
+      editable: true | false,
+      isContinuousPlay: true | false,
+      linkEndpoints: true | false,
+    },
+
+    zoomLevels: number[] | [500, 1000, 2000],
+    seekStyle: 'fill' | 'line',
+  }
+
+
+
+
+
+
+    function ReactStudioJS(arg1:TArgs,ee:any):any{}
+
+
+    export = ReactStudioJS
+}
